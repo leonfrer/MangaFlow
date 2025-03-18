640af8f8a4b066da9b2eb157f3fc103ed2cd2e56
Leon <leonfrer@gmail.com>
2025-03-18 10:21:02 +0800

    Initial Commit

---
+++ MangaFlow/MangaFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaFlowApp.swift
+//  MangaFlow
+//
+//  Created by Leon on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct MangaFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MangaFlowTests/MangaFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  MangaFlowTests.swift
+//  MangaFlowTests
+//
+//  Created by Leon on 2025-03-18.
+//
+
+import Testing
+@testable import MangaFlow
+
+struct MangaFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
